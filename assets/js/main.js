76c2610df95fbb597171ffdb14d45a57cf65d17c
SuperCoder <supercode@AirvonSuperCode.fritz.box>
2022-01-31 15:37:05 +0100

    1

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let fName = document.getElementById("fName");
+console.log(fName.value);
+
+let mail = document.getElementById("email");
+
+let message = document.getElementById("message");
+
+console.log("test")
+
+function getInfo() {
+    console.log(fName.value);
+}
